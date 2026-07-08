31050dfcb531cec503553cef44e24998c8652c3d
JDK - Jose Arias <jose.flores@jdoutstanding.com>
2026-07-08 12:28:55 -0600

    Add validation, CRUD endpoints, and logging enhancements
    Introduced FluentValidation with a custom MediatR ValidationBehavior and registered validators. Added CreateProductCommandValidator. Implemented Delete, Update, and GetProductByCategory endpoints with corresponding commands, handlers, and responses. Enhanced endpoints with 404 documentation and added logging to handlers. Updated project files with new dependencies and global usings.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Products.GetProductByCategory;
+
+namespace Catalog.API.Products.GetProductsByCategoryByCategory
+{
+    //public record GetProductByCategorysRequest();
+    public record GetProductByCategorysResponse(IEnumerable<Product> products);
+
+    public class GetProductsByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+                var response = result.Adapt<GetProductByCategorysResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByCategorysResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Producst by Category")
+            .WithDescription("Get Product by Category");
+        }
+    }
+}
